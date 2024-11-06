c068403433fca4a39c9cb0770a5f69416afb65bd
davibetoni <davibetoni@gmail.com>
2024-11-06 16:57:26 -0300

    adding forms on homepage and get_info view

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,8 +1,13 @@
 class HomepageController < ApplicationController
   def index
+  end
+
+  def get_info
     param1 = 'valor1'
     param2 = 'valor2'
 
-    # system("python3 app/jobs/esi/esi.py #{param1} #{param2}")
+    system("python3 app/jobs/esi/esi.py #{param1} #{param2}")
+
+    sleep(2)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'homepage#index'
+  post 'homepage/get_info', to: 'homepage#get_info', as: 'homepage_get_info'
 
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
